a5afede6993dd60275a3433d4b1178e22ba687fa
kevinmtz22 <107017688+kevinmtz22@users.noreply.github.com>
2024-03-06 14:35:46 -0600

    Escena Juego 2

--- Proyecto VR/Assets/Scripts/Rotar.cs
+++ Proyecto VR/Assets/Scripts/Rotar.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Rotar : MonoBehaviour
 {
-    
+    public string nombreAnimacion;
     public Animator animator;
 
-    public void ActivarRotacion()
+    public void ActivarRotacion(string nombreAnimacion)
     {
+        
         // Activa la animación "rotar" en el animator
-        animator.Play("rotar");
+        animator.Play(nombreAnimacion);
     }
+
 }
